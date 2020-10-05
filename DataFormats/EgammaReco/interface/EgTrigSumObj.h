d68dbed1470486728dfbe07588a25ed7f95ed0d0
Sam Harper <sam.j.harper@gmail.com>
2020-10-05 10:48:58 +0200

    code checks

--- DataFormats/EgammaReco/interface/EgTrigSumObj.h
+++ DataFormats/EgammaReco/interface/EgTrigSumObj.h
@@ -12,59 +12,57 @@
 #include <unordered_map>
 #include <string>
 
-namespace reco{
+namespace reco {
   class RecoEcalCandidate;
 }
 
-namespace reco{
+namespace reco {
   class EgTrigSumObj {
   public:
+    EgTrigSumObj() : energy_(0.), pt_(0.), eta_(0.), phi_(0.), hasPixelMatch_(false) {}
 
-    EgTrigSumObj():energy_(0.),pt_(0.),eta_(0.),phi_(0.),hasPixelMatch_(false){}
-
-    EgTrigSumObj(float energy,float pt,float eta,float phi);
+    EgTrigSumObj(float energy, float pt, float eta, float phi);
     EgTrigSumObj(const reco::RecoEcalCandidate& ecalCand);
-  
-    float energy()const{return energy_;}
-    float et()const{return pt();}
-    float pt()const{return pt_;}
-    float mass()const{return 0.;}
-    float eta()const{return eta_;}
-    float phi()const{return phi_;}
 
-    const reco::SuperClusterRef& superCluster()const{return superCluster_;}
-    const reco::GsfTrackRefVector& gsfTracks()const{return gsfTracks_;}
-    const reco::ElectronSeedRefVector& seeds()const{return seeds_;}
-   
-    void setSuperCluster(const reco::SuperClusterRef& sc){superCluster_ = sc;}
-    void setGsfTracks(const reco::GsfTrackRefVector&& trks){gsfTracks_ = trks;}   
+    float energy() const { return energy_; }
+    float et() const { return pt(); }
+    float pt() const { return pt_; }
+    float mass() const { return 0.; }
+    float eta() const { return eta_; }
+    float phi() const { return phi_; }
+
+    const reco::SuperClusterRef& superCluster() const { return superCluster_; }
+    const reco::GsfTrackRefVector& gsfTracks() const { return gsfTracks_; }
+    const reco::ElectronSeedRefVector& seeds() const { return seeds_; }
+
+    void setSuperCluster(const reco::SuperClusterRef& sc) { superCluster_ = sc; }
+    void setGsfTracks(const reco::GsfTrackRefVector&& trks) { gsfTracks_ = trks; }
     void setSeeds(const reco::ElectronSeedRefVector&& seeds);
 
-    bool hasVar(const std::string& varName)const; 
-    float var(const std::string& varName,bool raiseExcept=true)const; 
-    const std::unordered_map<std::string,float>& vars()const{return vars_;}
+    bool hasVar(const std::string& varName) const;
+    float var(const std::string& varName, bool raiseExcept = true) const;
+    const std::unordered_map<std::string, float>& vars() const { return vars_; }
     //varNames and varNamesStr are reasonably expensive functions and are more
     //intended for debugging than normal use
-    std::vector<std::string> varNames()const;
-    std::string varNamesStr()const;
-    void setVar(std::string name,float value,bool overwrite=true); 
-    void clearVars(){vars_.clear();}
+    std::vector<std::string> varNames() const;
+    std::string varNamesStr() const;
+    void setVar(std::string name, float value, bool overwrite = true);
+    void clearVars() { vars_.clear(); }
 
   private:
     float energy_;
     float pt_;
     float eta_;
-    float phi_; 
+    float phi_;
     bool hasPixelMatch_;
-    std::unordered_map<std::string,float> vars_;
-    
+    std::unordered_map<std::string, float> vars_;
+
     reco::SuperClusterRef superCluster_;
     reco::GsfTrackRefVector gsfTracks_;
     //currently these are pixel seeds but could be tracker seeds...
     reco::ElectronSeedRefVector seeds_;
-    
   };
 
-}
+}  // namespace reco
 
 #endif
