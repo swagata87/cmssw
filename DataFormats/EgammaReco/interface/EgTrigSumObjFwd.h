964d60fdad360502898047846a6f80a50d83b792
Sam Harper <sam.j.harper@gmail.com>
2020-12-14 15:46:17 +0100

    code formats and cleanups

--- DataFormats/EgammaReco/interface/EgTrigSumObjFwd.h
+++ DataFormats/EgammaReco/interface/EgTrigSumObjFwd.h
@@ -9,15 +9,10 @@
 namespace reco {
   class EgTrigSumObj;
 
-  /// collection of SuperCluser objectr
   typedef std::vector<EgTrigSumObj> EgTrigSumObjCollection;
-  /// reference to an object in a collection of EgTrigSumObj objects
   typedef edm::Ref<EgTrigSumObjCollection> EgTrigSumObjRef;
-  /// reference to a collection of EgTrigSumObj objects
   typedef edm::RefProd<EgTrigSumObjCollection> EgTrigSumObjRefProd;
-  /// vector of references to objects in the same colletion of EgTrigSumObj objects
   typedef edm::RefVector<EgTrigSumObjCollection> EgTrigSumObjRefVector;
-  /// iterator over a vector of reference to EgTrigSumObjs
   typedef EgTrigSumObjRefVector::iterator egTrigSumObj_iterator;
 }  // namespace reco
 
