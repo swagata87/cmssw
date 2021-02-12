59ac11b6a7713c9e2e0b89ba583c4d77c5166cf5
swmukher <s.mukherjee@cern.ch>
2021-02-12 10:28:59 +0100

    seed TICL by L1 e/gamma objects

---
+++ RecoHGCal/TICL/plugins/SeedingRegionByL1.h
@@ -0,0 +1,44 @@
+#ifndef RecoHGCal_TICL_SeedingRegionByL1_h
+#define RecoHGCal_TICL_SeedingRegionByL1_h
+#include <memory>
+#include <string>
+#include "RecoHGCal/TICL/plugins/SeedingRegionAlgoBase.h"
+
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Utilities/interface/ESGetToken.h"
+
+#include "DataFormats/L1TCorrelator/interface/TkEm.h"
+#include "DataFormats/L1TCorrelator/interface/TkEmFwd.h"
+
+namespace ticl {
+  class SeedingRegionByL1 final : public SeedingRegionAlgoBase {
+  public:
+    SeedingRegionByL1(const edm::ParameterSet& conf, edm::ConsumesCollector& sumes);
+    ~SeedingRegionByL1() override;
+
+    void initialize(const edm::EventSetup& es) override{};
+    void makeRegions(const edm::Event& ev, const edm::EventSetup& es, std::vector<TICLSeedingRegion>& result) override;
+    static void fillPSetDescription(edm::ParameterSetDescription& desc);
+    static edm::ParameterSetDescription makePSetDescription();
+
+  private:
+    edm::EDGetTokenT<std::vector<l1t::TkEm>> l1TkEmsToken_;
+    int algo_verbosity_=0;
+    double minPt_; // minimum pT of L1 TkEm objects
+    double minAbsEta_; // minimum |eta| of L1 TkEm objects
+    double maxAbsEta_; // maximum |eta| of L1 TkEm objects 
+    std::vector<double> endcapScalings_; // pT scaling factors for endcap
+    int quality_; // hwQual 
+
+    double TkEmOfflineEt(double Et, double Eta) const;
+
+  };
+}  // namespace ticl
+#endif
