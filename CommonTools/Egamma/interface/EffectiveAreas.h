e3ab70cfc6b43161fa927233cfd80a24ca024e59
shdutta16 <shdutta@cern.ch>
2022-10-21 10:09:29 +0200

    Modified EffectiveAreas class to read two effective area (EA) constants from EA constants file to implement quadratic pile-up correction

--- CommonTools/Egamma/interface/EffectiveAreas.h
+++ CommonTools/Egamma/interface/EffectiveAreas.h
@@ -8,13 +8,17 @@ class EffectiveAreas {
 public:
   // Constructor, destructor
   EffectiveAreas(const std::string& filename);
+  EffectiveAreas(const std::string& filename, const bool quadraticEAflag);
 
   // Accessors
   const float getEffectiveArea(float eta) const;
+  const float getLinearEA(float eta) const;
+  const float getQuadraticEA(float eta) const;
 
   // Utility functions
   void printEffectiveAreas() const;
   void checkConsistency() const;
+
 
 private:
   // Data members
@@ -22,6 +26,12 @@ private:
   std::vector<float> absEtaMin_;            // low limit of the eta range
   std::vector<float> absEtaMax_;            // upper limit of the eta range
   std::vector<float> effectiveAreaValues_;  // effective area for this eta range
+
+  // Following members are for quadratic PU-correction (introduced for cutBasedPhotonID in Run3_122X)
+  bool quadraticEAflag_;
+  std::vector<float> linearEffectiveAreaValues_;
+  std::vector<float> quadraticEffectiveAreaValues_;
+
 };
 
 #endif
